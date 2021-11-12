6073ce75e78eb8a9f7f27bab88f1f0a59fb9f598
aishwaryatiwari <51767393+aishwaryatiwari@users.noreply.github.com>
2021-11-11 23:19:33 -0800

    Update Common_SQL_Usecases.sql

--- Common_SQL_Usecases.sql
+++ Common_SQL_Usecases.sql
@@ -203,3 +203,37 @@ select case when id%2=0 then id-1
 , student
 from seat
 order by id asc;
+
+-- 22. Input: 
+--Customer table:
+--+-------------+-------------+
+--| customer_id | product_key |
+--+-------------+-------------+
+--| 1           | 5           |
+--| 2           | 6           |
+--| 3           | 5           |
+--| 3           | 6           |
+--| 1           | 6           |
+--+-------------+-------------+
+--Product table:
+--+-------------+
+--| product_key |
+--+-------------+
+--| 5           |
+--| 6           |
+--+-------------+
+--Output: 
+--+-------------+
+--| customer_id |
+--+-------------+
+--| 1           |
+--| 3           |
+--+-------------+
+--Explanation: 
+--The customers who bought all the products (5 and 6) are customers with IDs 1 and 3.
+
+select customer_id
+from customer
+group by customer_id
+having count(distinct product_key) = (select count(product_key) from product);
+
