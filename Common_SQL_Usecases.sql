5df3f1aeba5a3155e41eee07c1589dd39e8a4706
aishwaryatiwari <51767393+aishwaryatiwari@users.noreply.github.com>
2021-11-14 20:01:10 -0800

    Update Common_SQL_Usecases.sql

--- Common_SQL_Usecases.sql
+++ Common_SQL_Usecases.sql
@@ -253,3 +253,74 @@ FROM orders
 WHERE product_name in ('A','B')
 GROUP BY customer_id
 having count(distinct product_name) = 2;
+
+-- 25. Write an SQL query to find all the possible page recommendations for every user. Each recommendation should appear as a row in the result table with these columns:
+
+-- user_id: The ID of the user that your system is making the recommendation to.
+-- page_id: The ID of the page that will be recommended to user_id.
+-- friends_likes: The number of the friends of user_id that like page_id.
+
+--Input: 
+--Friendship table:
+--+----------+----------+
+--| user1_id | user2_id |
+--+----------+----------+
+--| 1        | 2        |
+--| 1        | 3        |
+--| 1        | 4        |
+--| 2        | 3        |
+--| 2        | 4        |
+--| 2        | 5        |
+--| 6        | 1        |
+--+----------+----------+
+--Likes table:
+--+---------+---------+
+--| user_id | page_id |
+--+---------+---------+
+--| 1       | 88      |
+--| 2       | 23      |
+--| 3       | 24      |
+--| 4       | 56      |
+--| 5       | 11      |
+--| 6       | 33      |
+--| 2       | 77      |
+--| 3       | 77      |
+--| 6       | 88      |
+--+---------+---------+
+--Output: 
+--+---------+---------+---------------+
+--| user_id | page_id | friends_likes |
+--+---------+---------+---------------+
+--| 1       | 77      | 2             |
+--| 1       | 23      | 1             |
+--| 1       | 24      | 1             |
+--| 1       | 56      | 1             |
+--| 1       | 33      | 1             |
+--| 2       | 24      | 1             |
+--| 2       | 56      | 1             |
+--| 2       | 11      | 1             |
+--| 2       | 88      | 1             |
+--| 3       | 88      | 1             |
+--| 3       | 23      | 1             |
+--| 4       | 88      | 1             |
+--| 4       | 77      | 1             |
+--| 4       | 23      | 1             |
+--| 5       | 77      | 1             |
+--| 5       | 23      | 1             |
+--+---------+---------+---------------+
+
+with user_frnd_page as (
+    SELECT a.user1_id, b.user_id, b.page_id # user, all user friends, page_id
+    FROM Friendship as a
+    JOIN Likes as b
+    ON a.user2_id=b.user_id
+    UNION 
+    SELECT a.user2_id,b.user_id,b.page_id
+    FROM Friendship as a
+    JOIN Likes as b
+    ON a.user1_id=b.user_id)
+select a.user1_id user_id, a.page_id, count(a.user_id) friends_likes
+from user_frnd_page a
+left join Likes b on a.user1_id = b.user_id and a.page_id = b.page_id
+where b.page_id is null
+group by a.user1_id, a.page_id ; 
