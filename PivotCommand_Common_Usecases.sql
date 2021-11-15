32a5c3174dd8ceab5714b53c48eca089c834c4bb
aishwaryatiwari <51767393+aishwaryatiwari@users.noreply.github.com>
2021-11-15 15:08:53 -0800

    Update PivotCommand_Common_Usecases.sql

--- PivotCommand_Common_Usecases.sql
+++ PivotCommand_Common_Usecases.sql
@@ -81,3 +81,46 @@ from occupations)job
 pivot (
 min(name) for occupation in (Doctor, Professor, Singer, Actor)
 )pvt;
+
+--Example 5
+-- Pivot does NOT work in MySQL. The following shows two methods of solving a pivot problem, on for MS SQL Server and one for MySQL
+
+-- Input: 
+-- Student table:
+-- +--------+-----------+
+-- | name   | continent |
+-- +--------+-----------+
+-- | Jane   | America   |
+-- | Pascal | Europe    |
+-- | Xi     | Asia      |
+-- | Jack   | America   |
+-- +--------+-----------+
+-- Output: 
+-- +---------+------+--------+
+-- | America | Asia | Europe |
+-- +---------+------+--------+
+-- | Jack    | Xi   | Pascal |
+-- | Jane    | null | null   |
+-- +---------+------+--------+
+
+-- MS SQL Server using PIVOT 
+
+select [America], [Asia], [Europe]
+from
+	(select name
+	, continent
+	, row_number() over (partition by continent order by name asc) as rn
+	from student)sub
+pivot
+	(max(name) for continent in ([America], [Asia], [Europe]))pvt;
+	
+-- MySQL using window functions - 
+SELECT
+        MAX(CASE WHEN continent = 'America' THEN name END )AS America,
+        MAX(CASE WHEN continent = 'Asia' THEN name END )AS Asia,
+        MAX(CASE WHEN continent = 'Europe' THEN name END )AS Europe  
+FROM (SELECT *
+      , ROW_NUMBER()OVER(PARTITION BY continent ORDER BY name) AS row_id 
+      FROM student) AS t
+GROUP BY row_id;
+	
