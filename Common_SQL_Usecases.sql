91184321fe5080c6a32b48f84979c79b93ec9acd
aishwaryatiwari <51767393+aishwaryatiwari@users.noreply.github.com>
2021-12-15 12:04:44 -0800

    Update Common_SQL_Usecases.sql

--- Common_SQL_Usecases.sql
+++ Common_SQL_Usecases.sql
@@ -459,3 +459,14 @@ from install_cte a
 left join Activity b on a.player_id = b.player_id and a.install_dt + 1 = b.event_date
 group by a.install_dt
 order by 1;
+
+--27. The following question wants information of all users in the system and the number of orders in 2019.
+-- While seemingly simple, it requires attention to the use of case vs where clause. 
+-- Instinctively using where clause will eliminate users who have no order for 2019, thus giving incorrect answer.
+
+select u.user_id as buyer_id, u.join_date, count(case when year(o.order_date) = '2019' then o.order_id
+                                                 else null end) as orders_in_2019
+from users u
+left join orders o on u.user_id = o.buyer_id
+left join items i on o.item_id = i.item_id
+group by u.user_id, u.join_date
