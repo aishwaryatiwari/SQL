4dd9de6d15115ba877661bb0fbb8644ade7d377c
aishwaryatiwari <51767393+aishwaryatiwari@users.noreply.github.com>
2021-12-15 21:31:50 -0800

    Update Common_SQL_Usecases.sql

--- Common_SQL_Usecases.sql
+++ Common_SQL_Usecases.sql
@@ -471,7 +471,7 @@ left join orders o on u.user_id = o.buyer_id
 left join items i on o.item_id = i.item_id
 group by u.user_id, u.join_date
 
---27. Generate rows based on a column value - 
+--28. Generate rows based on a column value - 
 -- Input: 
 -- Tasks table:
 -- +---------+----------------+
@@ -518,3 +518,68 @@ from cte a
 left join Executed b on a.task_id = b.task_id and a.subtask_id = b.subtask_id
 where b.subtask_id is null
 order by 1,2;
+
+
+--29. Input: Total budget of $70000
+-- A company wants to hire new employees. The budget of the company for the salaries is $70000. The company's criteria for hiring are:
+-- Hiring the largest number of seniors.
+-- After hiring the maximum number of seniors, use the remaining budget to hire the largest number of juniors.
+-- Candidates table:
+-- +-------------+------------+--------+
+-- | employee_id | experience | salary |
+-- +-------------+------------+--------+
+-- | 1           | Junior     | 10000  |
+-- | 9           | Junior     | 10000  |
+-- | 2           | Senior     | 20000  |
+-- | 11          | Senior     | 20000  |
+-- | 13          | Senior     | 50000  |
+-- | 4           | Junior     | 40000  |
+-- +-------------+------------+--------+
+-- Output: 
+-- +------------+---------------------+
+-- | experience | accepted_candidates |
+-- +------------+---------------------+
+-- | Senior     | 2                   |
+-- | Junior     | 2                   |
+-- +------------+---------------------+
+-- Explanation: 
+-- We can hire 2 seniors with IDs (2, 11). Since the budget is $70000 and the sum of their salaries is $40000, we still have $30000 but they are not enough to hire the senior candidate with ID 13.
+-- We can hire 2 juniors with IDs (1, 9). Since the remaining budget is $30000 and the sum of their salaries is $20000, we still have $10000 but they are not enough to hire the junior candidate with ID 4.
+-- 
+-- 
+-- Input: 
+-- Candidates table:
+-- +-------------+------------+--------+
+-- | employee_id | experience | salary |
+-- +-------------+------------+--------+
+-- | 1           | Junior     | 10000  |
+-- | 9           | Junior     | 10000  |
+-- | 2           | Senior     | 80000  |
+-- | 11          | Senior     | 80000  |
+-- | 13          | Senior     | 80000  |
+-- | 4           | Junior     | 40000  |
+-- +-------------+------------+--------+
+-- Output: 
+-- +------------+---------------------+
+-- | experience | accepted_candidates |
+-- +------------+---------------------+
+-- | Senior     | 0                   |
+-- | Junior     | 3                   |
+-- +------------+---------------------+
+-- Explanation: 
+-- We cannot hire any seniors with the current budget as we need at least $80000 to hire one senior.
+-- We can hire all three juniors with the remaining budget.
+
+
+with cte as (
+select employee_id, experience, salary, sum(salary) over (partition by experience order by salary asc, employee_id asc) as cum_sal
+from Candidates )
+select 'Senior' experience, count(employee_id) accepted_candidates
+from cte
+where experience = 'Senior'
+and cum_sal <=70000
+union 
+select 'Junior' experience, count(employee_id) accepted_candidates
+from cte
+where experience = 'Junior'
+and cum_sal <= (70000 - (select ifnull(max(cum_sal),0) from cte where experience = 'Senior' and cum_sal <=70000))
