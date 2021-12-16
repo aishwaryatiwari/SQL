85b23904ed7aa87cac877add7d071b95d884ebb7
aishwaryatiwari <51767393+aishwaryatiwari@users.noreply.github.com>
2021-12-15 17:46:12 -0800

    Update Gaps_and_Islands_example.sql

--- Gaps_and_Islands_example.sql
+++ Gaps_and_Islands_example.sql
@@ -62,3 +62,86 @@ from (
         from dbo.del)sub
     )sub2
 where sub2.cnt >= n;
+
+
+-- Example 3: 
+-- Table: Failed
+-- 
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | fail_date    | date    |
+-- +--------------+---------+
+-- fail_date is the primary key for this table.
+-- This table contains the days of failed tasks.
+--
+-- Table: Succeeded
+-- 
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | success_date | date    |
+-- +--------------+---------+
+-- success_date is the primary key for this table.
+-- This table contains the days of succeeded tasks.
+--  
+-- A system is running one task every day. Every task is independent of the previous tasks. The tasks can fail or succeed.
+-- Write an SQL query to generate a report of period_state for each continuous interval of days in the period from 2019-01-01 to 2019-12-31.
+-- period_state is 'failed' if tasks in this interval failed or 'succeeded' if tasks in this interval succeeded. Interval of days are retrieved as start_date and end_date.
+-- Return the result table ordered by start_date.
+-- The query result format is in the following example.
+--
+-- Input: 
+-- Failed table:
+-- +-------------------+
+-- | fail_date         |
+-- +-------------------+
+-- | 2018-12-28        |
+-- | 2018-12-29        |
+-- | 2019-01-04        |
+-- | 2019-01-05        |
+-- +-------------------+
+-- Succeeded table:
+-- +-------------------+
+-- | success_date      |
+-- +-------------------+
+-- | 2018-12-30        |
+-- | 2018-12-31        |
+-- | 2019-01-01        |
+-- | 2019-01-02        |
+-- | 2019-01-03        |
+-- | 2019-01-06        |
+-- +-------------------+
+-- Output: 
+-- +--------------+--------------+--------------+
+-- | period_state | start_date   | end_date     |
+-- +--------------+--------------+--------------+
+-- | succeeded    | 2019-01-01   | 2019-01-03   |
+-- | failed       | 2019-01-04   | 2019-01-05   |
+-- | succeeded    | 2019-01-06   | 2019-01-06   |
+-- +--------------+--------------+--------------+
+-- Explanation: 
+-- The report ignored the system state in 2018 as we care about the system in the period 2019-01-01 to 2019-12-31.
+-- From 2019-01-01 to 2019-01-03 all tasks succeeded and the system state was "succeeded".
+-- From 2019-01-04 to 2019-01-05 all tasks failed and the system state was "failed".
+-- From 2019-01-06 to 2019-01-06 all tasks succeeded and the system state was "succeeded".
+
+select state as period_state
+, min(dt) as start_date
+, max(dt) as end_date
+from (
+select state
+, dt
+, row_number() over (order by dt asc) - row_number() over (partition by state order by dt asc) as state_rn
+from (
+    select 'failed' state, fail_date dt
+    from failed
+    where fail_date between '2019-01-01' and '2019-12-31'
+    union 
+    select 'succeeded' state, success_date dt
+    from succeeded
+    where success_date between '2019-01-01' and '2019-12-31'
+    )sub 
+)sub2
+group by state, state_rn
+order by start_date asc 
