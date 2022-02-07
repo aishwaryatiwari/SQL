47730138794be5a365a2c7562ea36503618de634
aishwaryatiwari <51767393+aishwaryatiwari@users.noreply.github.com>
2022-02-06 20:17:54 -0800

    Update Common_SQL_Usecases.sql

--- Common_SQL_Usecases.sql
+++ Common_SQL_Usecases.sql
@@ -669,3 +669,60 @@ select user1_id, user2_id
 from new
 where common = (select max(common) from new)
 
+-- 33. Table: Friendship
+-- 
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | user1_id    | int  |
+-- | user2_id    | int  |
+-- +-------------+------+
+-- (user1_id, user2_id) is the primary key for this table.
+-- Each row of this table indicates that the users user1_id and user2_id are friends.
+-- Note that user1_id < user2_id.
+-- 
+-- A friendship between a pair of friends x and y is strong if x and y have at least three common friends. 
+-- Write an SQL query to find all the strong friendships. 
+-- Note that the result table should not contain duplicates with user1_id < user2_id.
+-- Example 1:
+-- 
+-- Input: 
+-- Friendship table:
+-- +----------+----------+
+-- | user1_id | user2_id |
+-- +----------+----------+
+-- | 1        | 2        |
+-- | 1        | 3        |
+-- | 2        | 3        |
+-- | 1        | 4        |
+-- | 2        | 4        |
+-- | 1        | 5        |
+-- | 2        | 5        |
+-- | 1        | 7        |
+-- | 3        | 7        |
+-- | 1        | 6        |
+-- | 3        | 6        |
+-- | 2        | 6        |
+-- +----------+----------+
+-- Output: 
+-- +----------+----------+---------------+
+-- | user1_id | user2_id | common_friend |
+-- +----------+----------+---------------+
+-- | 1        | 2        | 4             |
+-- | 1        | 3        | 3             |
+-- +----------+----------+---------------+
+-- Explanation: 
+-- Users 1 and 2 have 4 common friends (3, 4, 5, and 6).
+-- Users 1 and 3 have 3 common friends (2, 6, and 7).
+-- We did not include the friendship of users 2 and 3 because they only have two common friends (1 and 6).
+
+-- You might think you don't need this cte, but you do!
+with cte as(select user1_id,user2_id from friendship
+            union 
+            select user2_id,user1_id from friendship)
+select a.user1_id, b.user1_id user2_id, count(a.user2_id) common_friend 
+from cte a 
+left join cte b on a.user2_id = b.user2_id and a.user1_id < b.user1_id
+where (a.user1_id, b.user1_id) in (select * from Friendship)
+group by a.user1_id, b.user1_id
+having count(a.user2_id) >= 3;
