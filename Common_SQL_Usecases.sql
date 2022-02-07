c405a9b0948271ad82488197199d6a1311173b71
aishwaryatiwari <51767393+aishwaryatiwari@users.noreply.github.com>
2022-02-06 19:58:23 -0800

    Update Common_SQL_Usecases.sql

--- Common_SQL_Usecases.sql
+++ Common_SQL_Usecases.sql
@@ -347,7 +347,8 @@ WHERE product_name in ('A','B')
 GROUP BY customer_id
 having count(distinct product_name) = 2;
 
--- 25. Write an SQL query to find all the possible page recommendations for every user. Each recommendation should appear as a row in the result table with these columns:
+-- 25. Write an SQL query to find all the possible page recommendations for every user. Each recommendation should appear as a row in the result table 
+-- with these columns:
 
 -- user_id: The ID of the user that your system is making the recommendation to.
 -- page_id: The ID of the page that will be recommended to user_id.
@@ -543,8 +544,10 @@ order by 1,2;
 -- | Junior     | 2                   |
 -- +------------+---------------------+
 -- Explanation: 
--- We can hire 2 seniors with IDs (2, 11). Since the budget is $70000 and the sum of their salaries is $40000, we still have $30000 but they are not enough to hire the senior candidate with ID 13.
--- We can hire 2 juniors with IDs (1, 9). Since the remaining budget is $30000 and the sum of their salaries is $20000, we still have $10000 but they are not enough to hire the junior candidate with ID 4.
+-- We can hire 2 seniors with IDs (2, 11). Since the budget is $70000 and the sum of their salaries is $40000, we still have $30000 but they are not enough to hire the 
+-- senior candidate with ID 13.
+-- We can hire 2 juniors with IDs (1, 9). Since the remaining budget is $30000 and the sum of their salaries is $20000, we still have $10000 but they are not enough to 
+-- hire the junior candidate with ID 4.
 -- 
 -- 
 -- Input: 
@@ -613,3 +616,56 @@ from
 select data
 from freq_cte
 where freq = max_freq;
+
+-- 32. Table: Relations
+-- 
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | user_id     | int  |
+-- | follower_id | int  |
+-- +-------------+------+
+-- (user_id, follower_id) is the primary key for this table.
+-- Each row of this table indicates that the user with ID follower_id is following the user with ID user_id. 
+-- Write an SQL query to find all the pairs of users with the maximum number of common followers. In other words, 
+-- if the maximum number of common followers between any two users is maxCommon, then you have to return all pairs of users that have maxCommon common followers.
+-- The result table should contain the pairs user1_id and user2_id where user1_id < user2_id.
+
+-- Input: 
+-- Relations table:
+-- +---------+-------------+
+-- | user_id | follower_id |
+-- +---------+-------------+
+-- | 1       | 3           |
+-- | 2       | 3           |
+-- | 7       | 3           |
+-- | 1       | 4           |
+-- | 2       | 4           |
+-- | 7       | 4           |
+-- | 1       | 5           |
+-- | 2       | 6           |
+-- | 7       | 5           |
+-- +---------+-------------+
+-- Output: 
+-- +----------+----------+
+-- | user1_id | user2_id |
+-- +----------+----------+
+-- | 1        | 7        |
+-- +----------+----------+
+-- Explanation: 
+-- Users 1 and 2 have two common followers (3 and 4).
+-- Users 1 and 7 have three common followers (3, 4, and 5).
+-- Users 2 and 7 have two common followers (3 and 4).
+-- Since the maximum number of common followers between any two users is 3, we return all pairs of users with three common followers, which is only the pair (1, 7).
+-- We return the pair as (1, 7), not as (7, 1).
+-- Note that we do not have any information about the users that follow users 3, 4, and 5, so we consider them to have 0 followers.
+
+with new as (   select r1.user_id user1_id, r2.user_id user2_id, count(r1.follower_id) as common
+                from relations r1
+                join relations r2 on r1.user_id <r2.user_id  and r1.follower_id = r2.follower_id
+                group by r1.user_id, r2.user_id
+            )
+select user1_id, user2_id 
+from new
+where common = (select max(common) from new)
+
