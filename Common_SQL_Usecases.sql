4df7609fcdd48fd5ad955eb89c0acdb9a1e42cbd
aishwaryatiwari <51767393+aishwaryatiwari@users.noreply.github.com>
2022-02-09 22:08:40 -0800

    Update Common_SQL_Usecases.sql

--- Common_SQL_Usecases.sql
+++ Common_SQL_Usecases.sql
@@ -883,4 +883,72 @@ left join (select * from orders
 	   where dispatch_date >'2018-06-23' and dispatch_date <= '2019-06-23') o on a.book_id = o.book_id 
 where datediff('2019-06-23', a.available_from) > 30
 group by a.book_id, a.name
-having sum(coalesce(o.quantity,0)) < 10
+having sum(coalesce(o.quantity,0)) < 10;
+
+--36. https://leetcode.com/problems/last-person-to-fit-in-the-bus/
+-- Table: Queue
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | person_id   | int     |
+-- | person_name | varchar |
+-- | weight      | int     |
+-- | turn        | int     |
+-- +-------------+---------+
+-- person_id is the primary key column for this table.
+-- This table has the information about all people waiting for a bus.
+-- The person_id and turn columns will contain all numbers from 1 to n, where n is the number of rows in the table.
+-- turn determines the order of which the people will board the bus, where turn=1 denotes the first person to board and turn=n denotes the last person to board.
+-- weight is the weight of the person in kilograms.
+-- There is a queue of people waiting to board a bus. However, the bus has a weight limit of 1000 kilograms, so there may be some people who cannot board.
+-- 
+-- Write an SQL query to find the person_name of the last person that can fit on the bus without exceeding the weight limit. 
+-- The test cases are generated such that the first person does not exceed the weight limit.
+-- The query result format is in the following example.
+-- 
+-- Example 1:
+-- 
+-- Input: 
+-- Queue table:
+-- +-----------+-------------+--------+------+
+-- | person_id | person_name | weight | turn |
+-- +-----------+-------------+--------+------+
+-- | 5         | Alice       | 250    | 1    |
+-- | 4         | Bob         | 175    | 5    |
+-- | 3         | Alex        | 350    | 2    |
+-- | 6         | John Cena   | 400    | 3    |
+-- | 1         | Winston     | 500    | 6    |
+-- | 2         | Marie       | 200    | 4    |
+-- +-----------+-------------+--------+------+
+-- Output: 
+-- +-------------+
+-- | person_name |
+-- +-------------+
+-- | John Cena   |
+-- +-------------+
+-- Explanation: The folowing table is ordered by the turn for simplicity.
+-- +------+----+-----------+--------+--------------+
+-- | Turn | ID | Name      | Weight | Total Weight |
+-- +------+----+-----------+--------+--------------+
+-- | 1    | 5  | Alice     | 250    | 250          |
+-- | 2    | 3  | Alex      | 350    | 600          |
+-- | 3    | 6  | John Cena | 400    | 1000         | (last person to board)
+-- | 4    | 2  | Marie     | 200    | 1200         | (cannot board)
+-- | 5    | 4  | Bob       | 175    | ___          |
+-- | 6    | 1  | Winston   | 500    | ___          |
+-- +------+----+-----------+--------+--------------+
+
+-- The same question can be done with a limit or top function. 
+-- It can also be done with a rank function. 
+
+with cte as (
+    select *
+    , sum(weight) over (order by turn asc) as cumul_wt
+    from queue),
+cte2 as (
+    select max(cumul_wt) max_wt
+    from cte
+    where cumul_wt <=1000)
+select person_name 
+from cte
+where cumul_wt = (select max_wt from cte2);
